5e423f15aaa66909c64ef4adce74cdc40b9ea331
David Leal <davidvexel@gmail.com>
2019-07-21 14:30:03 -0500

    Adding default views

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,5 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::resource('/reservations', 'ReservationController');
+Route::resource('/reports', 'ReportsController');
