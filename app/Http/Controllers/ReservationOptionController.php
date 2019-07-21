e1528347d58717a1b1c2e74209fa704fba077f4a
David Leal <davidvexel@gmail.com>
2019-07-21 14:02:29 -0500

    Adding reservation options and seeder

---
+++ app/Http/Controllers/ReservationOptionController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\ReservationOption;
+use Illuminate\Http\Request;
+
+class ReservationOptionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\ReservationOption  $reservationOption
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ReservationOption $reservationOption)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\ReservationOption  $reservationOption
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ReservationOption $reservationOption)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\ReservationOption  $reservationOption
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ReservationOption $reservationOption)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\ReservationOption  $reservationOption
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ReservationOption $reservationOption)
+    {
+        //
+    }
+}
