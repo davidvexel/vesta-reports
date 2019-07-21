490b28936e440412a4cfe62ad2596f46199c7ae1
David Leal <davidvexel@gmail.com>
2019-07-21 14:38:07 -0500

    Adjusting navigation in admin

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -60,19 +60,6 @@
         </ul>
 
         <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
-                <a href="#" class="navbar-nav-link">
-                    Text link
-                </a>
-            </li>
-
-            <li class="nav-item dropdown">
-                <a href="#" class="navbar-nav-link">
-                    <i class="icon-bell2"></i>
-                    <span class="d-md-none ml-2">Notifications</span>
-                    <span class="badge badge-mark border-white ml-auto ml-md-0"></span>
-                </a>
-            </li>
 
             <li class="nav-item dropdown dropdown-user">
                 <a href="#" class="navbar-nav-link d-flex align-items-center dropdown-toggle" data-toggle="dropdown">
@@ -126,87 +113,36 @@
         <!-- Sidebar content -->
         <div class="sidebar-content">
 
-            <!-- User menu -->
-            <div class="sidebar-user">
-                <div class="card-body">
-                    <div class="media">
-                        <div class="mr-3">
-                            <a href="#"><img src="{{ asset('theme/global_assets/images/image.png') }}" width="38" height="38" class="rounded-circle" alt=""></a>
-                        </div>
-
-                        <div class="media-body">
-                            <div class="media-title font-weight-semibold">Victoria Baker</div>
-                            <div class="font-size-xs opacity-50">
-                                <i class="icon-pin font-size-sm"></i> &nbsp;Santa Ana, CA
-                            </div>
-                        </div>
-
-                        <div class="ml-3 align-self-center">
-                            <a href="#" class="text-white"><i class="icon-cog3"></i></a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!-- /user menu -->
-
-
             <!-- Main navigation -->
             <div class="card card-sidebar-mobile">
                 <ul class="nav nav-sidebar" data-nav-type="accordion">
 
                     <!-- Main -->
                     <li class="nav-item-header"><div class="text-uppercase font-size-xs line-height-xs">Main</div> <i class="icon-menu" title="Main"></i></li>
-                    <li class="nav-item">
-                        <a href="../full/index.html" class="nav-link">
+                    <li class="nav-item nav-item-open">
+                        <a href="{{ url('/reservations') }}" class="nav-link">
                             <i class="icon-home4"></i>
-                            <span>Dashboard</span>
+                            <span>Reservaciones</span>
                         </a>
-                    </li>
-                    <li class="nav-item nav-item-submenu nav-item-expanded nav-item-open">
-                        <a href="#" class="nav-link"><i class="icon-stack"></i> <span>Starter kit</span></a>
-
-                        <ul class="nav nav-group-sub" data-submenu-title="Starter kit">
-                            <li class="nav-item"><a href="../seed/layout_nav_horizontal.html" class="nav-link">Horizontal navigation</a></li>
-                            <li class="nav-item"><a href="../seed/sidebar_none.html" class="nav-link">No sidebar</a></li>
-                            <li class="nav-item"><a href="../seed/sidebar_main.html" class="nav-link active">1 sidebar</a></li>
-                            <li class="nav-item nav-item-submenu">
-                                <a href="#" class="nav-link">2 sidebars</a>
-                                <ul class="nav nav-group-sub">
-                                    <li class="nav-item"><a href="../seed/sidebar_secondary.html" class="nav-link">Secondary sidebar</a></li>
-                                    <li class="nav-item"><a href="../seed/sidebar_right.html" class="nav-link">Right sidebar</a></li>
-                                </ul>
-                            </li>
-                            <li class="nav-item nav-item-submenu">
-                                <a href="#" class="nav-link">3 sidebars</a>
-                                <ul class="nav nav-group-sub">
-                                    <li class="nav-item"><a href="../seed/sidebar_right_hidden.html" class="nav-link">Right sidebar hidden</a></li>
-                                    <li class="nav-item"><a href="../seed/sidebar_right_visible.html" class="nav-link">Right sidebar visible</a></li>
-                                </ul>
-                            </li>
-                            <li class="nav-item nav-item-submenu">
-                                <a href="#" class="nav-link">Content sidebars</a>
-                                <ul class="nav nav-group-sub">
-                                    <li class="nav-item"><a href="../seed/sidebar_content_left.html" class="nav-link">Left sidebar</a></li>
-                                    <li class="nav-item"><a href="../seed/sidebar_content_right.html" class="nav-link">Right sidebar</a></li>
-                                </ul>
-                            </li>
-                            <li class="nav-item"><a href="../seed/layout_boxed.html" class="nav-link">Boxed layout</a></li>
-                            <li class="nav-item-divider"></li>
-                            <li class="nav-item"><a href="../seed/navbar_fixed_main.html" class="nav-link">Fixed main navbar</a></li>
-                            <li class="nav-item"><a href="../seed/navbar_fixed_secondary.html" class="nav-link">Fixed secondary navbar</a></li>
-                            <li class="nav-item"><a href="../seed/navbar_fixed_both.html" class="nav-link">Both navbars fixed</a></li>
-                            <li class="nav-item"><a href="../seed/layout_fixed.html" class="nav-link">Fixed layout</a></li>
-                        </ul>
                     </li>
                     <li class="nav-item">
-                        <a href="../full/changelog.html" class="nav-link">
-                            <i class="icon-list-unordered"></i>
-                            <span>Changelog</span>
-                            <span class="badge bg-blue-400 align-self-center ml-auto">2.2</span>
+                        <a href="{{ url('/import-report') }}" class="nav-link">
+                            <i class="icon-home4"></i>
+                            <span>Importar Reporte</span>
                         </a>
                     </li>
-                    <!-- /main -->
-
+                    <li class="nav-item">
+                        <a href="{{ url('/clients') }}" class="nav-link">
+                            <i class="icon-home4"></i>
+                            <span>Clientes</span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="{{ url('/clients') }}" class="nav-link">
+                            <i class="icon-home4"></i>
+                            <span>Usuarios</span>
+                        </a>
+                    </li>
                 </ul>
             </div>
             <!-- /main navigation -->
